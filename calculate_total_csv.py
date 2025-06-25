57b89faba62a9a6b1574e4f16e740d6eecaadb19
smrutivssutmca <40881123+smrutivssutmca@users.noreply.github.com>
2025-06-25 21:18:20 +0530

    Create calculate_total_csv.py

---
+++ calculate_total_csv.py
@@ -0,0 +1,25 @@
+
+import csv
+
+input_file = 'user_input.csv'
+output_file = 'user_totals.csv'
+
+user_total = {}
+
+with open(input_file, 'r') as csv_file:
+    data = csv.reader(csv_file)
+    
+    for row in data:
+        user_id = data["user_id"]
+        amount = data["amount"]
+        
+        user_total[user_id] += amount
+        
+with open(output_file, 'w') as csv_file:
+    data = csv.writer(csv_file)
+    
+    data.writerow(['user_id', 'total_amount'])
+    
+    for user_id, amount in user_total.items():
+        data.writerow([user_id, amount])
+        
