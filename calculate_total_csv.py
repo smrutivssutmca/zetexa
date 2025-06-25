8a41a92a453d81d15f7dee84d2b63ba577ce2e13
smrutivssutmca <40881123+smrutivssutmca@users.noreply.github.com>
2025-06-25 21:26:02 +0530

    Update calculate_total_csv.py
    Added error exception

--- calculate_total_csv.py
+++ calculate_total_csv.py
@@ -6,20 +6,28 @@ output_file = 'user_totals.csv'
 
 user_total = {}
 
-with open(input_file, 'r') as csv_file:
-    data = csv.reader(csv_file)
-    
-    for row in data:
-        user_id = data["user_id"]
-        amount = data["amount"]
+try:
+    with open(input_file, 'r') as csv_file:
+        data = csv.reader(csv_file)
         
-        user_total[user_id] += amount
+        for row in data:
+            user_id = data["user_id"]
+            amount = data["amount"]
+            
+            user_total[user_id] += amount
+except Exception as e:
+    print("Error occoured while reading the file : ", str(e))
         
-with open(output_file, 'w') as csv_file:
-    data = csv.writer(csv_file)
-    
-    data.writerow(['user_id', 'total_amount'])
-    
-    for user_id, amount in user_total.items():
-        data.writerow([user_id, amount])
+
+try:
+    with open(output_file, 'w') as csv_file:
+        data = csv.writer(csv_file)
         
+        data.writerow(['user_id', 'total_amount'])
+        
+        for user_id, amount in user_total.items():
+            data.writerow([user_id, amount])
+except Exception as e:
+    print("Error occoured while reading the file : ", str(e))
+        
+    
